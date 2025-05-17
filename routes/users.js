380fc40fd50e5a696bc7b8a83026ae73e1853df0
Nitin <nk17oct@gmail.com>
2025-05-17 17:53:35 +0530

    change method

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,6 @@ const users = require("../controllers/users");
 router.route("/test").get((req, res) => {
   return res.status(200).send("6702957c2a68d28a33bd7fae");
 });
-router.route("/register").post(users.register);
+router.route("/register").get(users.register);
 
 module.exports = router;
