032f0ddea6355e55f20d8b50550a66c598b62e7b
Nitin <nk17oct@gmail.com>
2025-05-18 00:14:41 +0530

    send json

--- controllers/users.js
+++ controllers/users.js
@@ -3,5 +3,5 @@ const User = require("../models/User");
 module.exports.register = async (req, res) => {
   const user = new User();
   await user.save();
-  res.status(201).send(user._id);
+  res.status(201).json({ user: user._id });
 };
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const users = require("../controllers/users");
 
 router.route("/test").get((req, res) => {
-  return res.status(200).send("6702957c2a68d28a33bd7fae");
+  return res.status(200).json({ user: "6702957c2a68d28a33bd7fae" });
 });
 router.route("/register").get(users.register);
 
